ba7e5bfcfa1263fdded946b8f29c17066ce363c3
Chandini-syed <chandusyed@gmail.com>
2022-07-29 21:07:37 +0530

    Adding 10th file

---
+++ multiple_operations.js
@@ -0,0 +1,8 @@
+//function myFunction(a, b, c, d, e, f) {
+//    return (((a + b - c) * d) / e) ** f;
+//}
+
+function myFunction(a, b, c, d, e, f) {
+   
+    return Math.pow(((((a+b)-c)*d)/e),f)
+ }
