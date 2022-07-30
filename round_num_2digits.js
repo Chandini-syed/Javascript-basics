1895d3a9e2f414bf5e4889d66473735a1b780b62
Chandini-syed <chandusyed@gmail.com>
2022-07-30 16:58:20 +0530

    Adding 18th file

---
+++ round_num_2digits.js
@@ -0,0 +1,16 @@
+// Write a function that takes a number (a) as argument
+// Round a to the 2nd digit after the comma
+// Return the rounded number
+function
+myFunction
+(
+a
+)
+{
+let b=Number(a)
+return Number(b.toFixed(2))
+}
+
+// function myFunction(a) {
+//     return Number(a.toFixed(2));
+//   }
